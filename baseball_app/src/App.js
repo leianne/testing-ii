7e60c4b51c253f237267ea88e6985fdb931e55f0
leianne louis <leilouis112@gmail.com>
2019-03-05 16:38:16 -0500

    implemented the strike functionality

--- baseball_app/src/App.js
+++ baseball_app/src/App.js
@@ -2,16 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import players from './dummyData';
-import DashboardView from './dashboard/DashBoardView';
+import DashBoardComponent from './dashboard/DashBoardComponent';
 
+const methods = ['foul', 'strike', 'ball', 'hit']
 class App extends Component {
   state = {
-    players: players
+    players: players,
+    selectedPlayerId: null, 
+    currPlayer: {}
   }
+
+  playerSelected = (id) => {
+    const player = this.state.players.find(e => e.id === id)
+    this.setState({
+      ...this.state,
+      selectedPlayerId: id,
+      currPlayer: player
+    })
+    
+  }
+
+  handleClick = (evt, e) => {
+    evt.preventDefault();
+    if(e === 'strike' && this.state.currPlayer.strikes < 2) {
+      this.setState({
+        ...this.state,
+        currPlayer: {
+          ...this.state.currPlayer,
+          strikes: ++this.state.currPlayer.strikes
+        }
+      })
+    }
+  }
+
   render() {
+    console.log(this.state.currPlayer)
     return (
-      <div className="App">
-        <DashboardView players={this.state.players}/>
+     <div className="App">
+         <DashBoardComponent handleClick={this.handleClick} methods={methods} playerSelected={this.playerSelected} players={this.state.players} />
       </div>
     );
   }
--- baseball_app/src/dashboard/DashBoardComponent.js
+++ baseball_app/src/dashboard/DashBoardComponent.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
 const DashBoardComponent = props => {
+    console.log(props)
     if(props.players && props.players.length > 0){
         return (
             <>
                 <h1>Players</h1>
                 {props.players.map(e => {
                     return (
-                        <div key={e.id} onClick={(evt) => props.playerSelected(evt) }>
+                        <div key={e.id} onClick={() => props.playerSelected(e.id) }>
                             {e.player_name}
                         </div>
+                    )
+                })}
+                {props.methods.map(e => {
+                    return (
+                        <button key={e} onClick={(evt) => props.handleClick(evt, e)}>{e.toUpperCase()}</button>
                     )
                 })}
             </>
